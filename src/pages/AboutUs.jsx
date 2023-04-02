0f6dad96957161a5fbd09ab2309dee6f76f131b7
eskander-taher <eskander.alshaibany@gmail.com>
2023-04-02 23:34:43 +0500

    add about text

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Section from "../components/UI/section/Section";
+import { Typography, Stack } from "@mui/material";
 
-
-
-export default function AboutUs(){
-
-    return(
-        <Section header="About Us">
-            
-        </Section>
-    )
+export default function AboutUs() {
+  return (
+    <Section container header="About Us">
+      <Stack>
+        <Typography fontSize={{md: "2rem", xs:"1rem"}}>
+          Welcome to cereSoftware! We are a software company that specializes in
+          developing cutting-edge technology solutions to meet the unique needs
+          of our clients. Our team of talented developers and designers are
+          dedicated to delivering high-quality software products and services
+          that are designed to help businesses of all sizes stay ahead of the
+          competition.
+          <br />
+          <br />
+          At cereSoftware, we pride ourselves on our commitment to innovation
+          and excellence. We are constantly exploring new technologies and
+          techniques to ensure that our solutions are always at the forefront of
+          the industry.
+          <br />
+          <br />
+          Our focus on quality and customer satisfaction is what sets us apart
+          from the competition. Whether you are looking for a customized
+          software solution, a mobile app, or a web-based platform, our team has
+          the expertise and experience to deliver. We work closely with our
+          clients to ensure that their unique needs and requirements are met,
+          and we always strive to exceed expectations.
+          <br />
+          <br />
+          If you are looking for a trusted partner to help take your business to
+          the next level, look no further than cereSoftware. Contact us today to
+          learn more about how we can help you achieve your goals.
+        </Typography>
+      </Stack>
+    </Section>
+  );
 }
